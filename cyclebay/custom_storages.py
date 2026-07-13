7473e71eb5399c661ca2d7a1f789b7629730a0b2
officialfounderjourney-cell <officialfounderjourney@gmail.com>
2026-07-13 15:36:48 -0400

    configurar variables de entorno para producción

--- cyclebay/custom_storages.py
+++ cyclebay/custom_storages.py
@@ -1,10 +1,62 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
+from django.core.files.base import File
+from django.core.files.storage import Storage
+from vercel.blob import BlobClient
+import io
+import os
 
 
-class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+class VercelBlobMediaStorage(Storage):
+    def __init__(self):
+        self.client = BlobClient()
+        self.base_path = settings.MEDIAFILES_LOCATION
 
+    def _full_path(self, name):
+        return f"{self.base_path}/{name}"
 
-class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    def _save(self, name, content):
+        path = self._full_path(name)
+        if content.multiple_chunks():
+            content.seek(0)
+            self.client.put(path, content.read(), access="public")
+        else:
+            content.seek(0)
+            self.client.put(path, content.read(), access="public")
+        return name
+
+    def _open(self, name, mode="rb"):
+        path = self._full_path(name)
+        blob = self.client.get(path)
+        return File(io.BytesIO(blob), name)
+
+    def url(self, name):
+        path = self._full_path(name)
+        try:
+            meta = self.client.head(path)
+            return meta.url
+        except Exception:
+            return ""
+
+    def delete(self, name):
+        path = self._full_path(name)
+        try:
+            meta = self.client.head(path)
+            self.client.delete([meta.url])
+        except Exception:
+            pass
+
+    def exists(self, name):
+        path = self._full_path(name)
+        try:
+            self.client.head(path)
+            return True
+        except Exception:
+            return False
+
+    def size(self, name):
+        path = self._full_path(name)
+        try:
+            meta = self.client.head(path)
+            return meta.size
+        except Exception:
+            return 0
--- cyclebay/settings.py
+++ cyclebay/settings.py
@@ -47,9 +47,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 INTERNAL_IPS = os.getenv("INTERNAL_IPS", [])
 
-ALLOWED_HOSTS = [
-    if ON_VERCEL:
-    ALLOWED_HOSTS = ['*']  # O tu dominio específico
+if ON_VERCEL:
+    ALLOWED_HOSTS = ['*']
 else:
     ALLOWED_HOSTS = [
         "127.0.0.1",
@@ -87,7 +86,7 @@ INSTALLED_APPS = [
     # Other apps
     "crispy_forms",
     "crispy_bootstrap4",
-    "storages",
+
 ]
 
 MIDDLEWARE = [
@@ -200,41 +199,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-if not DEVELOPMENT:
-    # aws settings
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = 'eu-west-1'  # Ireland
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
+MEDIAFILES_LOCATION = 'media'
 
-    # s3 static settings
-    STATICFILES_LOCATION = 'static'
-    # URL path for your static files where they will be served from
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    STATICFILES_STORAGE = 'cyclebay.custom_storages.StaticStorage'
-
-    # s3 media settings
-    MEDIAFILES_LOCATION = 'media'
-    # URL path for media files where they will be served from
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'cyclebay.custom_storages.MediaStorage'
-else:
-    # URL path for your static files where they
-    # will be served from during development
+if DEVELOPMENT:
     STATIC_URL = '/static/'
-    # Dir where static files will be collected using
-    # python manage.py collectstatic
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    # URL path for media files where they will be served from
     MEDIA_URL = '/media/'
-    # Dir where media files are stored during development
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+    MEDIA_URL = '/media/'
+    DEFAULT_FILE_STORAGE = 'cyclebay.custom_storages.VercelBlobMediaStorage'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
